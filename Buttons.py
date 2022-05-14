4e2dd73453d3efbee94a067489ebaa5c60edf4a5
jewelm21 <jewel_medel@brown.edu>
2022-05-14 10:42:07 -0400

    Jewel-2 hours -only debugging the buttons file to render in mainfile

--- Buttons.py
+++ Buttons.py
@@ -3,12 +3,12 @@
 class Button:
 	def __init__(self, text, image, x, y, font, color):
 		self.text_in = text
+		self.font = font
+		self.color = color
 		self.text = self.font.render(self.text_in, True, self.color)
 		self.image = image
 		self.x = x
 		self.y = y
-		self.font = font
-		self.color = color
 		self.rect = self.image.get_rect(center=(self.x, self.y))
 		self.text_rect = self.text.get_rect(center=(self.x, self.y))
 
